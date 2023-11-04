b3f71ed5ffabade7250053978c7866ac8e0ab930
JamesHagan-prog <112729585+JamesHagan-prog@users.noreply.github.com>
2023-11-04 10:28:19 -0700

    VARS

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 using namespace std;
 
+#include <cstdlib>
 #include <iostream>
 #include <ctime>
 
+const int MAX_X_SIZE = 9;
+const int MAX_Y_SIZE = 12;
+
 int main (void) {
+    char gameBoard[MAX_X_SIZE][MAX_Y_SIZE]; //Gameboard
 
 
 
-    
+
 }
