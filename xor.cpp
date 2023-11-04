749118e3be3487ff534ffb47ab08cf141ac967d2
Nicholas <aganirabn@gmail.com>
2023-11-04 11:42:34 -0700

    update

--- xor.cpp
+++ xor.cpp
@@ -1,26 +1,34 @@
 #include "xor.h"
 
-
-
 void playGame() {
-
+  int xVal = 9;
+  printMenu(xVal);
 }
 
-void printMenu() {
+void printMenu(int &xVal) {
+  std::string userInput = ".";
+  std::cout
+      << "\n\n\n\n\n\n\n\n____________________________________________\n       "
+         "   Welcome to XOR\n_____________________________________________\n"
+      << "The name of the game is to prevent the enemeis (shown as "
+         "\"X's)\nfrom getting to the bottom of the board\nYour ship (shown as "
+         "\"^\" can move side to side and also has the ability to shoot and "
+         "destroyed enemies in its column\nEvery move or fire action takes a "
+         "turn, and beware the enemies move one row closer to the bottom after "
+         "every turn!\n if they reach the bottom you lose!)\n";
 
+  while (userInput == ".") {
+    std::cout << "Press Any Key To Start!";
+    std::cin >> userInput;
+  }
 
+  std::cout << "Difficulty | Easy - Medium - Hard |";
 }
 
+void displayBoard() {}
 
-void displayBoard() {
+void runTurn() {}
 
-}
+// bool validateMove(char choice) {
 
-
-void runTurn() {
-
-}
-
-bool validateMove(char choice) {
-
-}
+// }
--- xor.h
+++ xor.h
@@ -5,10 +5,12 @@
 #include <iostream>
 #include <ctime>
 
+using namespace std;
 
+const int Y_SIZE = 12;
 
 void playGame();
-void printMenu();
+void printMenu(int &xVal);
 void displayBoard();
 void runTurn();
 bool validateMove(char choice);
