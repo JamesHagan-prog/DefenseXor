a3e45d7e8b3aaed6affa312c5b2b40476df21cc3
JamesHagan-prog <112729585+JamesHagan-prog@users.noreply.github.com>
2023-11-04 14:45:03 -0700

    Static Done

--- xor.h
+++ xor.h
@@ -9,12 +9,12 @@ using namespace std;
 
 const int MAX_Y_SIZE = 12; // Default
 
-void setupBoard(char gameBoard[][MAX_Y_SIZE], int xSize);
+void setupBoard(char gameBoard[][MAX_Y_SIZE], int xSize, int &score);
 void printMenu(int &xVal);
 void playGame();
 void displayBoard(char gameBoard[][MAX_Y_SIZE], int score, int highScore,
                   int xSize);
-void runTurn(char gameBoard[][MAX_Y_SIZE]);
+void runTurn(int xSize, char gameBoard[][MAX_Y_SIZE], int &score);
 bool validateMove(char choice, int, int);
 void fire(int playerPos, char gameBoard[][MAX_Y_SIZE], int &score);
 int findPlayerPosition(char gameBoard[][MAX_Y_SIZE], int);
